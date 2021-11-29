c4fe4e95b0afcfa2f1f4673369906a056a19c172
Marriane791 <annemarrieakeyo42@gmail.com>
2021-11-30 00:56:38 +0300

    complete header of the clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA3xDOJD2BszagMwAiofqLOBouVO0C_asw",
+    authDomain: "challenge-c089a.firebaseapp.com",
+    projectId: "challenge-c089a",
+    storageBucket: "challenge-c089a.appspot.com",
+    messagingSenderId: "172867205330",
+    appId: "1:172867205330:web:0e8c17ccbd77273742550b",
+    measurementId: "G-0S950P2NXV"
+  };
